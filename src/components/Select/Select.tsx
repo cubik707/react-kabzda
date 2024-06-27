aa900e1451c9e81aba475c3f1b65c77e21e969a7
Vladislava <stdemidovez@gmail.com>
2024-06-27 15:34:26 +0300

    15-select

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -1,4 +1,4 @@
-import {useState, KeyboardEvent} from "react";
+import {useState, KeyboardEvent, useEffect} from "react";
 import s from  './Select.module.css'
 
 type ItemType = {
@@ -16,6 +16,10 @@ export function Select(props: SelectPropsType) {
     const [isOpen, setIsOpen] = useState(false);
     const [hoveredElementValue, setHoveredElementValue ] = useState(props.value)
 
+    useEffect(() => {
+        setHoveredElementValue(props.value)
+    }, [ props.value ]);
+
     const handleSelect = (value: any) => {
         props.onChange(value);
         setIsOpen(false);
@@ -24,27 +28,36 @@ export function Select(props: SelectPropsType) {
     const toggleItems = () => setIsOpen(!isOpen)
 
     const selectedItem = props.items.find((i) => i.value === props.value);
+    const hoveredItem = props.items.find(i=> i.value === hoveredElementValue)
 
     const onKeyUp = (e:  KeyboardEvent<HTMLDivElement>) =>{
-        for(let i=0;i<props.items.length; i++){
-            if(props.items[i].value === hoveredElementValue) {
-                if(props.items[i++]){
-                    props.onChange(props.items[i++].value)
+        if(e.key === "ArrowDown" || e.key === "ArrowUp"){
+            for(let i=0;i<props.items.length; i++){
+                if(props.items[i].value === hoveredElementValue) {
+                    const pretendentElement = e.key === "ArrowDown" ? props.items[i+1] : props.items[i-1]
+                    if(pretendentElement){
+                        props.onChange(pretendentElement.value)
+                    }
+                    return
                 }
-                break
             }
+            props.onChange(props.items[0].value)
         }
+        if(e.key === "Enter" || e.key === "Escape"){
+            setIsOpen(false)
+        }
+
     }
 
     return (
-        <div onKeyUp={onKeyUp} tabIndex={0}>
+        <div onKeyUp={onKeyUp} tabIndex={0} onKeyPress={()=>{}}>
             <h3 className={`${s.selectedItem} ${isOpen ? s.selectedItemOpen : ''}`} onClick={toggleItems}>
                 {selectedItem && selectedItem.title}
             </h3>
             {isOpen && (
                 <div className={s.items}>
                     {props.items.map(i => (
-                        <div className={s.item}
+                        <div className={s.item + " " + (hoveredItem === i ? s.selected : '')}
                              key={i.value}
                              onMouseEnter={()=>{
                                  setHoveredElementValue(i.value)
